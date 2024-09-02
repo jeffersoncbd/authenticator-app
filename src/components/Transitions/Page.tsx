6719776e10556e1fe6f1326ada0fc9d6554818c4
Jefferson Carlos <jeff.carlosbd@gmail.com>
2024-09-02 18:26:07 -0300

    possibilita delay na transição

--- src/components/Transitions/Page.tsx
+++ src/components/Transitions/Page.tsx
@@ -1,18 +1,20 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { motion, MotionStyle } from 'framer-motion'
 
 interface PageTransitionProperties {
   children: React.ReactNode
+  delay?: number
+  style?: MotionStyle
 }
 
-const PageTransition: React.FC<PageTransitionProperties> = ({ children }) => {
+const PageTransition: React.FC<PageTransitionProperties> = ({ children, delay }) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 2 }}
+      transition={{ duration: 2, delay }}
       style={{ height: "100%" }}
     >
       {children}
