42d432a7dbd4ddca7b85591a973d2f8881369174
Jefferson Carlos <jeff.carlosbd@gmail.com>
2024-09-02 19:03:41 -0300

    adiciona transição na listagem de grupos de permissões

--- src/app/app/applications/[id]/page.tsx
+++ src/app/app/applications/[id]/page.tsx
@@ -86,64 +86,66 @@ const Application: React.FC<ApplicationProperties> = (properties) => {
               </Flex>
             </PageTransition>
           )
-          : sortByKey(groups, (item) => item.name).map((group) => (
-            <Card
-              key={group.id}
-              onClick={() => {
-                if (detailedGroup === undefined) {
-                  setDetailedGroup(group.id)
-                } else {
-                  setDetailedGroup(undefined)
-                }
-              }}
-              style={{ cursor: 'pointer' }}
-            >
-              <Flex gap="2">
-                <ChevronRight
-                  style={{
-                    transform: detailedGroup === group.id ? "rotate(90deg)" : undefined,
-                    transition: "transform 0.2s"
-                  }}
-                />
-                <Heading as="h4" size="3" >{group.name}</Heading>
-              </Flex>
-              <ScrollArea style={{
-                maxHeight: detailedGroup === group.id ? "160px" : "0px",
-                paddingRight: "16px",
-                transition: "max-height 0.5s"
-              }}>
-                <Separator size="4" my="2" />
-                <Flex justify="between" mb="2">
-                  Permissões:
-                  <Flex gap="2">
-                    <Tooltip content="Read">
-                      <div style={{ width: "16px", textAlign: "center" }}>R</div>
-                    </Tooltip>
-                    <Tooltip content="Update">
-                      <div style={{ width: "16px", textAlign: "center" }}>U</div>
-                    </Tooltip>
-                    <Tooltip content="Delete">
-                      <div style={{ width: "16px", textAlign: "center" }}>D</div>
-                    </Tooltip>
-                  </Flex>
+          : sortByKey(groups, (item) => item.name).map((group, i) => (
+            <PageTransition key={group.id} delay={i / 5}>
+              <Card
+                key={group.id}
+                onClick={() => {
+                  if (detailedGroup === undefined) {
+                    setDetailedGroup(group.id)
+                  } else {
+                    setDetailedGroup(undefined)
+                  }
+                }}
+                style={{ cursor: 'pointer' }}
+              >
+                <Flex gap="2">
+                  <ChevronRight
+                    style={{
+                      transform: detailedGroup === group.id ? "rotate(90deg)" : undefined,
+                      transition: "transform 0.2s"
+                    }}
+                  />
+                  <Heading as="h4" size="3" >{group.name}</Heading>
                 </Flex>
-                <ul style={{ paddingLeft: "24px" }}>
-                  {Object.keys(group.permissions).map((key) => (
-                    <li key={key}>
-                      <Flex justify="between">
-                        {key}
-                        <Flex gap="2">
-                          {((group.permissions[key] as any) >>> 0).toString(2).split("").reverse()
-                            .map((permission, i) =>
-                              <Checkbox key={i} checked={permission === "1"} />
-                            )}
+                <ScrollArea style={{
+                  maxHeight: detailedGroup === group.id ? "160px" : "0px",
+                  paddingRight: "16px",
+                  transition: "max-height 0.5s"
+                }}>
+                  <Separator size="4" my="2" />
+                  <Flex justify="between" mb="2">
+                    Permissões:
+                    <Flex gap="2">
+                      <Tooltip content="Read">
+                        <div style={{ width: "16px", textAlign: "center" }}>R</div>
+                      </Tooltip>
+                      <Tooltip content="Update">
+                        <div style={{ width: "16px", textAlign: "center" }}>U</div>
+                      </Tooltip>
+                      <Tooltip content="Delete">
+                        <div style={{ width: "16px", textAlign: "center" }}>D</div>
+                      </Tooltip>
+                    </Flex>
+                  </Flex>
+                  <ul style={{ paddingLeft: "24px" }}>
+                    {Object.keys(group.permissions).map((key) => (
+                      <li key={key}>
+                        <Flex justify="between">
+                          {key}
+                          <Flex gap="2">
+                            {((group.permissions[key] as any) >>> 0).toString(2).split("").reverse()
+                              .map((permission, i) =>
+                                <Checkbox key={i} checked={permission === "1"} />
+                              )}
+                          </Flex>
                         </Flex>
-                      </Flex>
-                    </li>
-                  ))}
-                </ul>
-              </ScrollArea>
-            </Card>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </Card>
+            </PageTransition>
           )
           )}
       </Flex>
